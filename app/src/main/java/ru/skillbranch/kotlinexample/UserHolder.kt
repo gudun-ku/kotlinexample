4c10af6faf890da524c15c2f5101d1b610a45e7d
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-12-12 18:53:43 +0300

    Started to make homework

--- app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -3,14 +3,38 @@ package ru.skillbranch.kotlinexample
 object UserHolder {
     private val map = mutableMapOf<String, User>()
 
+    fun clear() {
+        map.clear()
+    }
+
     fun registerUser(
         fullName: String,
         email: String,
         password: String
     ): User {
         return User.makeUser(fullName,email= email,password = password)
-            .also { user -> map[user.login] = user }
+            .also {
+                checkUserAndSave(user)
+            }
+
     }
+
+    fun registerUserByPhone(fullName: String, rawPhone: String):User {
+        return User.makeUser(fullName,rawPhone=rawPhone)
+            .also {
+                checkUserAndSave(user)
+            }
+    }
+
+    fun checkUserAndSave(user: User, byPhone: Boolean = false) {
+        if (map[user.login]) = null
+            map[user.login] = user
+        else {
+            val msg = if(byPhone) "A user with this phone already exists" else "A user with this email already exists"
+            throw IllegalArgumentException(msg)
+        }
+    }
+
 
     fun loginUser (login: String, password: String) : String? {
         return map[login.trim()]?.run {
